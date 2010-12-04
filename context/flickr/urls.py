70bd94e46ea27cfd49896e618b1b9260145ae447
Joe Stump <joe@joestump.net>
2010-12-04 16:32:28 -0700

    Added ability to lookup by metro score. Added some debug.

--- context/flickr/urls.py
+++ context/flickr/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     (r'^(?P<id>[\d]+).html$', 'flickr.views.photo'),
+    (r'^metro_score/(?P<score>[\d]+).html$', 'flickr.views.metro_score'),
+    (r'^debug.html', 'flickr.views.debug'),
 )
--- context/flickr/views.py
+++ context/flickr/views.py
@@ -4,8 +4,23 @@ from django.shortcuts import render_to_response, get_object_or_404
 # Project imports
 from context.flickr.models import Photo, Tag, Category, Type, Feature
 
+
 def photo(request, id):
     return render_to_response('flickr/photo.html', {
         'photo': get_object_or_404(Photo, id=id)
     })
 
+
+def metro_score(request, score):
+    return render_to_response('flickr/results.html', {
+        'photos': Photo.objects.filter(metro_score=score)
+    })
+   
+
+def debug(request):
+    unknown = Category.objects.get(name='')
+    features = Feature.objects.filter(category=unknown)
+
+    return render_to_response('flickr/debug.html', {
+        'features': features
+    })
