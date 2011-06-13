c95ed639b634e574b9ccb41d985a0de9384b754a
Ben Standefer <benstandefer@gmail.com>
2011-06-13 23:27:15 +0000

    Add query limiting so popular places don't bomb out.  sqlite3 has an inefficient implementation of SELECT COUNT(*) so that was causing pagination to bomb out at high volume.

--- context/flickr/views.py
+++ context/flickr/views.py
@@ -1,5 +1,6 @@
 # Django imports
 from django.shortcuts import render_to_response, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Project imports
 from context.flickr.models import Photo, Tag, Category, Type, Feature
@@ -9,7 +10,6 @@ def photo(request, id):
     return render_to_response('flickr/photo.html', {
         'photo': get_object_or_404(Photo, id=id)
     })
-
 
 def metro_score(request, score):
     return render_to_response('flickr/results.html', {
@@ -23,9 +23,21 @@ def types(request):
 
 def type(request, type):
     type = get_object_or_404(Type, pk=type)
+
+    features_list = Feature.objects.filter(type=type).order_by('name')
+    paginator = Paginator(features_list, 500)
+    try:
+        page = int(request.GET.get('page', '1'))
+    except ValueError:
+        page = 1
+    try:
+        paginator_page = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        paginator_page = paginator.page(paginator.num_pages)
+
     return render_to_response('flickr/type.html', {
         'type': type,
-        'features': Feature.objects.filter(type=type).order_by('name')
+        'paginator_page': paginator_page
     })
 
 def categories(request):
@@ -35,16 +47,41 @@ def categories(request):
 
 def category(request, category):
     category = get_object_or_404(Category, pk=category)
+
+    feature_list = Feature.objects.filter(category=category).order_by('name')
+    paginator = Paginator(feature_list, 500)
+    try:
+        page = int(request.GET.get('page', '1'))
+    except ValueError:
+        page = 1
+    try:
+        paginator_page = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        paginator_page = paginator.page(paginator.num_pages)
+
     return render_to_response('flickr/category.html', {
         'category': category,
-        'features': Feature.objects.filter(category=category).order_by('name')
+        'paginator_page': paginator_page
     })
 
 def features(request, handle):
     feature = get_object_or_404(Feature, pk=handle)
-    return render_to_response('flickr/results.html', {
+
+    # Cast QuerySet to a list right off the bat because sqlite implements COUNT(*) as full table scan.
+    photo_list = list(Photo.objects.filter(features=feature)[:1000])
+    paginator = Paginator(photo_list, 100)
+    try:
+        page = int(request.GET.get('page', '1'))
+    except ValueError:
+        page = 1
+    try:
+        paginator_page = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        paginator_page = paginator.page(paginator.num_pages)
+
+    return render_to_response('flickr/features.html', {
         'feature': feature,
-        'photos': Photo.objects.filter(features=feature)
+        'paginator_page': paginator_page
     })
 
 
